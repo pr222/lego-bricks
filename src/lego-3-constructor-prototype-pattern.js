ca262383291845a230df07cc56d49430026d171a
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-15 19:51:47 +0200

    Complete constructor-prototype pattern

--- src/lego-3-constructor-prototype-pattern.js
+++ src/lego-3-constructor-prototype-pattern.js
@@ -19,8 +19,16 @@
  * @param {number} [y=4] - The vertical number of knobs of the brick.
  * @param {string} [color='red'] - Color of the brick ('blue', 'red', 'green', etc.).
  */
-export function LegoBrick (x, y, color) {
-  // TODO: Write your code here.
+export function LegoBrick (x = 2, y = 4, color = 'red') {
+  this.x = x
+  this.y = y
+  this.color = color
 }
 
-// TODO: Write your code here.
+LegoBrick.prototype.toString = function () {
+  return `${'® '.repeat(this.x).trim()}\n`.repeat(this.y).trim()
+}
+
+LegoBrick.prototype.render = function () {
+  console.log(this.toString())
+}
